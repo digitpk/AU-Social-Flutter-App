595b6e2d0102c7089d0e7719ad54d12ede49570a
Rahul Babu <rahulblatz002@gmail.com>
2020-06-28 15:24:16 +0530

    Event Details Page UI is completed

--- lib/pages/event_details_page.dart
+++ lib/pages/event_details_page.dart
@@ -1,6 +1,8 @@
 import 'package:ausocial/constants.dart';
+import 'package:ausocial/widgets/fancy_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EventDetailsPage extends StatelessWidget {
   final String eventTitle;
@@ -30,54 +32,167 @@ class EventDetailsPage extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            child: Image.network(
-              mediaUrl,
-              fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: FancyButton(
+                color: Color(primaryBlue),
+                label: 'Contact',
+                onPress: () {},
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: paddingInDetailPage,
-            ),
-            child: Text(eventTitle),
-          ),
-          Row(
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Stack(
             children: <Widget>[
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('Time'),
-                  Text(eventTime),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Text('Date'),
-                  Text(eventDate),
+                  Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                      mediaUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: paddingInDetailPage,
+                      top: paddingInDetailPage,
+                    ),
+                    child: Text(
+                      eventTitle,
+                      style: GoogleFonts.quicksand(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20.0,
+                      left: 15.0,
+                      right: 30,
+                      bottom: 20.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Time',
+                              style: GoogleFonts.quicksand(
+                                color: Colors.grey[500],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                eventTime,
+                                style: GoogleFonts.quicksand(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Date',
+                              style: GoogleFonts.quicksand(
+                                color: Colors.grey[500],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                eventDate,
+                                style: GoogleFonts.quicksand(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 1,
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20.0,
+                      left: paddingInDetailPage,
+                    ),
+                    child: Container(
+                      child: Text(
+                        eventDesc,
+                        style: GoogleFonts.quicksand(
+                          fontSize: 16,
+                          color: Colors.grey[500],
+                          fontWeight: FontWeight.w500,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 6,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: paddingInDetailPage,
+                      top: 20.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Department Venue ',
+                          style: GoogleFonts.quicksand(
+                            color: Colors.grey[500],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            department,
+                            style: GoogleFonts.quicksand(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ],
           ),
-          Divider(),
-          Container(
-            child: Text(
-              eventDesc,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Text('Department Venue '),
-          Text(
-            department,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
+        ),
       ),
     );
   }
